0d5539f1f76fe4273004793ccf3bba2d95b326f2
Rajat Agrawal <rajatatibm@gmail.com>
2023-04-06 12:22:16 +0100

    feat: fscloud-conformity

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@
 ##############################################################################
 
 locals {
-  kp_backup_crn        = var.backup_encryption_key_crn != null ? var.backup_encryption_key_crn : var.kms_key_crn
   auto_scaling_enabled = var.auto_scaling == null ? [] : [1]
   kms_service = var.kms_key_crn != null ? (
     can(regex(".*kms.*", var.kms_key_crn)) ? "kms" : (
@@ -44,7 +43,7 @@ resource "ibm_database" "postgresql_db" {
   configuration     = var.configuration != null ? jsonencode(var.configuration) : null
 
   key_protect_key           = var.kms_key_crn
-  backup_encryption_key_crn = local.kp_backup_crn
+  backup_encryption_key_crn = var.backup_encryption_key_crn
 
   dynamic "allowlist" {
     for_each = (var.allowlist != null ? var.allowlist : [])
