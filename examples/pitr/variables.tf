6b094facb27c614e41cf406f2600c206053cd17e
rajatagarwal-ibm <108140212+rajatagarwal-ibm@users.noreply.github.com>
2023-04-11 09:29:26 +0100

    feat: added support for hyper protect + support to create the IAM auth policy with KMS + added new fscloud profile module (#120)
    BREAKING CHANGE:
    - `key_protect_key_crn` renamed to `kms_key_crn` since it now support Hyper Protect

--- examples/pitr/variables.tf
+++ examples/pitr/variables.tf
@@ -13,7 +13,7 @@ variable "region" {
 variable "prefix" {
   type        = string
   description = "Prefix to append to all resources created by this example"
-  default     = "pg"
+  default     = "pg-pitr"
 }
 
 variable "resource_group" {
