0b20212e362b8d6bdc33df1fd0b07f0a11aac9e2
Md-Anam-Raihan <md.anam.raihan@ibm.com>
2023-04-17 16:27:49 +0530

    fix: resolved comments

--- examples/replica/outputs.tf
+++ examples/replica/outputs.tf
@@ -9,15 +9,10 @@ output "id" {
 
 output "replica_id" {
   description = "Postgresql read-only replica instance id"
-  value       = module.read_only_replica_postgresql_db[0].id
+  value       = module.read_only_replica_postgresql_db[*].id
 }
 
 output "version" {
   description = "Postgresql instance version"
   value       = module.postgresql_db.version
-}
-
-output "replica_version" {
-  description = "Postgresql read-only replica instance version"
-  value       = module.read_only_replica_postgresql_db[0].version
 }
