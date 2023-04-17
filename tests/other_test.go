3f614698f8c897f38153ee9e0f75fd5b2f3eb012
Md-Anam-Raihan <md.anam.raihan@ibm.com>
2023-04-18 05:25:48 +0530

    tests: moved autoscale and replica test

--- tests/other_test.go
+++ tests/other_test.go
@@ -8,6 +8,8 @@ import (
 )
 
 const restoredTerraformDir = "examples/backup"
+const replicaExampleTerraformDir = "examples/replica"
+const autoscaleExampleTerraformDir = "examples/autoscale"
 
 func TestRunRestoredDBExample(t *testing.T) {
 	t.Parallel()
@@ -21,4 +23,45 @@ func TestRunRestoredDBExample(t *testing.T) {
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
 	assert.NotNil(t, output, "Expected some output")
+}
+
+func TestRunAutoscaleExample(t *testing.T) {
+	t.Parallel()
+
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:            t,
+		TerraformDir:       autoscaleExampleTerraformDir,
+		Prefix:             "pg-autoscale",
+		BestRegionYAMLPath: regionSelectionPath,
+	})
+
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+}
+
+func testRunReplicaExample(t *testing.T, version string) {
+	t.Parallel()
+
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:            t,
+		TerraformDir:       replicaExampleTerraformDir,
+		Prefix:             "pg-replica",
+		BestRegionYAMLPath: regionSelectionPath,
+		TerraformVars: map[string]interface{}{
+			"pg_version": version,
+		},
+	})
+
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+}
+
+func TestRunReplicaExample(t *testing.T) {
+	t.Parallel()
+	versions := []string{"11", "12", "13", "14"}
+	for _, version := range versions {
+		t.Run(version, func(t *testing.T) { testRunReplicaExample(t, version) })
+	}
 }
--- tests/pr_test.go
+++ tests/pr_test.go
@@ -11,10 +11,8 @@ import (
 )
 
 const defaultExampleTerraformDir = "examples/default"
-const autoscaleExampleTerraformDir = "examples/autoscale"
 const fsCloudTerraformDir = "examples/fscloud"
 const completeExampleTerraformDir = "examples/complete"
-const replicaExampleTerraformDir = "examples/replica"
 const pitrTerraformDir = "examples/pitr"
 
 // Restricting due to limited availability of BYOK in certain regions
@@ -42,47 +40,6 @@ func TestRunDefaultExample(t *testing.T) {
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
 	assert.NotNil(t, output, "Expected some output")
-}
-
-func TestRunAutoscaleExample(t *testing.T) {
-	t.Parallel()
-
-	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
-		Testing:            t,
-		TerraformDir:       autoscaleExampleTerraformDir,
-		Prefix:             "pg-autoscale",
-		BestRegionYAMLPath: regionSelectionPath,
-	})
-
-	output, err := options.RunTestConsistency()
-	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
-}
-
-func testRunReplicaExample(t *testing.T, version string) {
-	t.Parallel()
-
-	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
-		Testing:            t,
-		TerraformDir:       replicaExampleTerraformDir,
-		Prefix:             "pg-replica",
-		BestRegionYAMLPath: regionSelectionPath,
-		TerraformVars: map[string]interface{}{
-			"pg_version": version,
-		},
-	})
-
-	output, err := options.RunTestConsistency()
-	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
-}
-
-func TestRunReplicaExample(t *testing.T) {
-	t.Parallel()
-	versions := []string{"11", "12", "13", "14"}
-	for _, version := range versions {
-		t.Run(version, func(t *testing.T) { testRunReplicaExample(t, version) })
-	}
 }
 
 func TestRunFSCloudExample(t *testing.T) {
