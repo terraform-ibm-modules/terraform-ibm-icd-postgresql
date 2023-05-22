1efa6b2ccc566144fb2f5cbf743cab9de7900f25
Matthew Lemmond <Matthew.Lemmond@ibm.com>
2023-05-22 09:34:34 -0400

    refactor: update variable description

--- variables.tf
+++ variables.tf
@@ -103,7 +103,7 @@ variable "resource_tags" {
 
 variable "access_tags" {
   type        = list(string)
-  description = "A list of access tags to apply to the resources created by the module, see https://cloud.ibm.com/docs/account?topic=account-access-tags-tutorial for more details"
+  description = "A list of access tags to apply to the PostgreSQL instance created by the module, see https://cloud.ibm.com/docs/account?topic=account-access-tags-tutorial for more details"
   default     = []
 
   validation {
