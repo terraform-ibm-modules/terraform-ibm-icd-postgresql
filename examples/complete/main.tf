1d4cafc4ef170cda1d21c7db32e4fbb9d0cbbd46
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2023-10-20 20:56:12 +0000

    fix(deps): update terraform terraform-ibm-modules/cbr/ibm to v1.15.1

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -68,7 +68,7 @@ resource "time_sleep" "wait_30_seconds" {
 
 module "cbr_zone" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.15.0"
+  version          = "1.15.1"
   name             = "${var.prefix}-VPC-network-zone"
   zone_description = "CBR Network zone representing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
