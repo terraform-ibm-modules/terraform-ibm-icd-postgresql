44560a2fefda9513fa52999a4047783cd8777a18
Rajat Agrawal <rajatatibm@gmail.com>
2023-10-31 13:56:58 +0000

    feat: remove plan validation argument

--- variables.tf
+++ variables.tf
@@ -12,12 +12,6 @@ variable "name" {
   description = "The name to give the Postgresql instance."
 }
 
-variable "plan_validation" {
-  type        = bool
-  description = "Enable or disable validating the database parameters for PostgreSQL during the plan phase."
-  default     = true
-}
-
 variable "remote_leader_crn" {
   type        = string
   description = "A CRN of the leader database to make the replica(read-only) deployment. The leader database is created by a database deployment with the same service ID. A read-only replica is set up to replicate all of your data from the leader deployment to the replica deployment by using asynchronous replication. For more information, see https://cloud.ibm.com/docs/databases-for-postgresql?topic=databases-for-postgresql-read-only-replicas"
