147ed29aa45494aed0b2aa6f39597f71e8a20381
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-02-27 12:12:21 +0530

    test: add new test to check for stable version support

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/cloudinfo"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
@@ -25,8 +26,11 @@ const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-res
 
 var permanentResources map[string]interface{}
 
+var sharedInfoSvc *cloudinfo.CloudInfoService
+
 // TestMain will be run before any parallel tests, used to read data from yaml for use with tests
 func TestMain(m *testing.M) {
+	sharedInfoSvc, _ = cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
 
 	var err error
 	permanentResources, err = common.LoadMapFromYaml(yamlLocation)
@@ -58,6 +62,7 @@ func TestRunFSCloudExample(t *testing.T) {
 			"kms_key_crn":                permanentResources["hpcs_south_root_key_crn"],
 			"pg_version":                 "15", // Always lock this test into the latest supported Postgres version
 		},
+		CloudInfoService: sharedInfoSvc,
 	})
 	options.SkipTestTearDown = true
 	output, err := options.RunTestConsistency()
@@ -97,6 +102,7 @@ func TestRunUpgradeCompleteExample(t *testing.T) {
 			},
 			"admin_pass": randomPass,
 		},
+		CloudInfoService: sharedInfoSvc,
 	})
 
 	output, err := options.RunTestUpgrade()
