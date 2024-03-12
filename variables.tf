f8a7a8b4143effa48ea5e5ebeb5b3e22195a3da4
kierra.searle@ibm.com <kierra.searle@ibm.com>
2024-03-12 17:46:04 -0400

    docs: host_flavor documentation update

--- variables.tf
+++ variables.tf
@@ -63,7 +63,7 @@ variable "member_cpu_count" {
 
 variable "member_host_flavor" {
   type        = string
-  description = "Allocated host flavor per member."
+  description = "Allocated host flavor per member. For more information, see https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/resources/database#host_flavor"
   default     = null
   # Validation is done in the Terraform plan phase by the IBM provider, so no need to add extra validation here.
 }
