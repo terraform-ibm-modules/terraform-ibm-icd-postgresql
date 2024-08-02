46044a77b810680d982149a0b41d4f6f10483983
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2024-08-02 18:24:28 +0530

    fix: reverted default wal_level value

--- variables.tf
+++ variables.tf
@@ -200,8 +200,8 @@ variable "configuration" {
   }
 
   validation {
-    condition     = var.configuration != null ? (var.configuration["wal_level"] != null ? contains(["hot_standby", "logical", "replica"], var.configuration["wal_level"]) : true) : true
-    error_message = "Value for `configuration[\"wal_level\"]` must be either `hot_standby`, `replica` or `logical`, if specified."
+    condition     = var.configuration != null ? (var.configuration["wal_level"] != null ? contains(["hot_standby", "logical"], var.configuration["wal_level"]) : true) : true
+    error_message = "Value for `configuration[\"wal_level\"]` must be either `hot_standby` or `logical`, if specified."
   }
 
   validation {
