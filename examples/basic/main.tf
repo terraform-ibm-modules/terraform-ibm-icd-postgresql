a1ab78d2288ca4ce2319da10161f88e553b88d57
shemau <shemau@gmail.com>
2024-11-10 19:40:18 +0000

    fix: logic when host flavour not set (#516)
    * fix: logic when host flavour not set
    
    * fix: update read replica to new hosting with min spec

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -45,16 +45,17 @@ resource "time_sleep" "wait_time" {
 ##############################################################################
 
 module "read_only_replica_postgresql_db" {
-  count             = var.read_only_replicas_count
-  source            = "../.."
-  resource_group_id = module.resource_group.resource_group_id
-  name              = "${var.prefix}-read-only-replica-${count.index}"
-  region            = var.region
-  resource_tags     = var.resource_tags
-  access_tags       = var.access_tags
-  pg_version        = var.pg_version
-  remote_leader_crn = module.postgresql_db.crn
-  member_memory_mb  = 2304  # Must be an increment of 384 megabytes. The minimum size of a read-only replica is 2 GB RAM
-  member_disk_mb    = 15360 # Must be an increment of 512 megabytes. The minimum size of a read-only replica is 15.36 GB of disk
-  depends_on        = [time_sleep.wait_time]
+  count              = var.read_only_replicas_count
+  source             = "../.."
+  resource_group_id  = module.resource_group.resource_group_id
+  name               = "${var.prefix}-read-only-replica-${count.index}"
+  region             = var.region
+  resource_tags      = var.resource_tags
+  access_tags        = var.access_tags
+  pg_version         = var.pg_version
+  remote_leader_crn  = module.postgresql_db.crn
+  member_host_flavor = "multitenant"
+  member_memory_mb   = 4096  # Must be an increment of 384 megabytes. The minimum size of a read-only replica is 2 GB RAM, new hosting model minimum is 4 GB RAM.
+  member_disk_mb     = 15360 # Must be an increment of 512 megabytes. The minimum size of a read-only replica is 15.36 GB of disk
+  depends_on         = [time_sleep.wait_time]
 }
