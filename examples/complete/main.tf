793285d29ebc7a425c65c57b75b89a3a506ff47e
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-12 21:47:07 +0000

    fix(deps): update terraform-module

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -21,7 +21,7 @@ locals {
 
 module "key_protect_all_inclusive" {
   source            = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version           = "4.21.6"
+  version           = "4.21.7"
   resource_group_id = module.resource_group.resource_group_id
   # Note: Database instance and Key Protect must be created in the same region when using BYOK
   # See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-key-protect&interface=ui#key-byok
@@ -58,7 +58,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.22.7"
+  version           = "7.22.8"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
