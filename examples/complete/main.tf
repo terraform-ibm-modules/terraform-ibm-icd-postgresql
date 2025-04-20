8bf679f4c11dee8f6bd4f6401bc77ee38186cab0
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-20 11:23:10 +0000

    chore(deps): update terraform terraform-ibm-modules/landing-zone-vpc/ibm to v7.22.13

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -58,7 +58,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "7.22.12"
+  version           = "7.22.13"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
