82bd931574382383f5ae747b29d00138d652a019
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2025-05-16 12:00:22 +0100

    Update main.tf

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -16,6 +16,9 @@ module "resource_group" {
 
 module "database" {
   source             = "../.."
+  # remove the above line and uncomment the below 2 lines to consume the module from the registry
+  # source            = "terraform-ibm-modules/icd-postgresql/ibm"
+  # version           = "X.Y.Z" # Replace "X.Y.Z" with a release version to lock into a specific release
   resource_group_id  = module.resource_group.resource_group_id
   name               = "${var.prefix}-data-store"
   pg_version         = var.pg_version
