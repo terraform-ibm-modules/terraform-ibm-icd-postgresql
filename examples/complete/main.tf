0e158dcd4249205239cef3f1b19777d055193df2
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-11 04:14:41 +0000

    fix(deps): update dependencies (#802)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -58,7 +58,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.10.5"
+  version           = "8.10.6"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
