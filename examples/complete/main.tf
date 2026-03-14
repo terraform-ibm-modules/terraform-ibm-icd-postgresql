e417ae4d9462f8b0136255df5f35dbf7fa24ca69
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-03-14 09:48:06 +0530

    chore(deps): update dependencies (#846)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -178,7 +178,7 @@ resource "time_sleep" "wait_120_seconds" {
 
 module "vpe" {
   source  = "terraform-ibm-modules/vpe-gateway/ibm"
-  version = "5.0.7"
+  version = "5.1.0"
   prefix  = "vpe-to-pg"
   cloud_service_by_crn = [
     {
