13a74ab793891d70ced6f75c3f1fdb771536e595
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-03-15 05:44:51 +0530

    chore(deps): update terraform terraform-ibm-modules/landing-zone-vpc/ibm to v8.15.11 (#849)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -58,7 +58,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "vpc" {
   source            = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version           = "8.15.10"
+  version           = "8.15.11"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
   prefix            = var.prefix
