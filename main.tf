4b31c2f42c56f59a2bb48184e1a1b7d51f20df66
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-06-16 11:30:53 +0530

    update common utility version

--- main.tf
+++ main.tf
@@ -166,14 +166,14 @@ resource "time_sleep" "wait_for_backup_kms_authorization_policy" {
 ########################################################################################################################
 
 # Workaround:
-# Montreal does not have ICD classic endpoint, so default to Toronto. This stops the module erroring,
+# The latest release of common-utility https://github.com/terraform-ibm-modules/terraform-ibm-common-utilities/releases/tag/v1.7.0
+# takes care of the endpoint unavailability of Montreal region. It fallbacks to Toronto region and then us-south region.
 # but it gets the classic versions and not the gen2 versions.
 # This MAY be wrong and result in an apply time failure IF the user specifies gen2 and an unsupported version.
-# ref: https://github.com/terraform-ibm-modules/terraform-ibm-common-utilities/issues/157
 module "available_versions" {
   source   = "terraform-ibm-modules/common-utilities/ibm//modules/icd-versions"
-  version  = "1.6.1"
-  region   = var.region == "ca-mon" ? "ca-tor" : var.region
+  version  = "1.7.0"
+  region   = var.region
   icd_type = "postgresql"
 }
 
