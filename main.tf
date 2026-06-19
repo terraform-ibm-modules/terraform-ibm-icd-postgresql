6da087321fc757331a209ce6cf0be18dc6b2d5a7
Vipin Kumar <77929205+vkuma17@users.noreply.github.com>
2026-06-19 12:57:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -166,10 +166,7 @@ resource "time_sleep" "wait_for_backup_kms_authorization_policy" {
 ########################################################################################################################
 
 # Workaround:
-# Montreal does not have ICD classic endpoint, so default to Toronto. This stops the module erroring,
-# but it gets the classic versions and not the gen2 versions.
-# This MAY be wrong and result in an apply time failure IF the user specifies gen2 and an unsupported version.
-# ref: https://github.com/terraform-ibm-modules/terraform-ibm-common-utilities/issues/157
+# Montreal does not have ICD classic endpoint, so common-utilities submodule defaults to Toronto for Gen1 Databases. This stops the module erroring.
 module "available_versions" {
   source   = "terraform-ibm-modules/common-utilities/ibm//modules/icd-versions"
   version  = "1.8.0"
