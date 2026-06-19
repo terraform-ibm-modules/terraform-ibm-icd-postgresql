1f802cae23b5bf866bf4241ccec6a811b5b9639c
Vipin Kumar <kumarvipin88073@gmail.com>
2026-06-19 12:42:53 +0530

    fix: new common-utilities version for gen2 database validation

--- main.tf
+++ main.tf
@@ -172,9 +172,11 @@ resource "time_sleep" "wait_for_backup_kms_authorization_policy" {
 # ref: https://github.com/terraform-ibm-modules/terraform-ibm-common-utilities/issues/157
 module "available_versions" {
   source   = "terraform-ibm-modules/common-utilities/ibm//modules/icd-versions"
-  version  = "1.6.1"
-  region   = var.region == "ca-mon" ? "ca-tor" : var.region
+  version  = "1.8.0"
+  region   = var.region
   icd_type = "postgresql"
+  plan     = var.plan
+  service  = "databases-for-postgresql"
 }
 
 locals {
--- tests/pr_test.go
+++ tests/pr_test.go
@@ -46,21 +46,7 @@ var validICDRegions = []string{
 	"us-south",
 }
 
-func GetRegionVersions(region string) (string, string) {
-
-	cloudInfoSvc, err := cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{
-		IcdRegion: region,
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	icdAvailableVersions, err := cloudInfoSvc.GetAvailableIcdVersions(icdType)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+func GetLatestAndOldestVersions(icdAvailableVersions []string) (string, string) {
 
 	if len(icdAvailableVersions) == 0 {
 		log.Fatal("No available ICD versions found")
@@ -94,25 +80,70 @@ func GetRegionVersions(region string) (string, string) {
 	oldestVersion := icdAvailableVersions[0]
 
 	return latestVersion, oldestVersion
+
+}
+
+func GetRegionVersions(region string) (string, string) {
+
+	icdRegion := region
+	if region == "ca-mon" {
+		icdRegion = "ca-tor"
+	}
+
+	cloudInfoSvc, err := cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{
+		IcdRegion: icdRegion,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	icdAvailableVersions, err := cloudInfoSvc.GetAvailableIcdVersions(icdType)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return GetLatestAndOldestVersions(icdAvailableVersions)
+}
+
+func GetVersionsGen2(region string, plan string) (string, string) {
+
+	cloudInfoSvc, err := cloudinfo.NewCloudInfoServiceFromEnv("TF_VAR_ibmcloud_api_key", cloudinfo.CloudInfoServiceOptions{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	icdAvailableVersions, err := cloudInfoSvc.GetAvailableIcdVersionsGen2("databases-for-postgresql", plan, region) // this function takes service, plan and region as arguments in this specific order
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return GetLatestAndOldestVersions(icdAvailableVersions)
 }
 
 func TestRunBasicGen2Example(t *testing.T) {
 	t.Parallel()
 
+	latestVersion, _ := GetVersionsGen2("ca-mon", "standard-gen2")
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
 		Testing:            t,
 		TerraformDir:       "examples/basic",
 		Prefix:             "pg-gen2",
 		BestRegionYAMLPath: regionSelectionPath,
 		ResourceGroup:      resourceGroup,
-		TerraformVars: map[string]interface{}{ // Limited gen2 to Montreal and version 18
+		TerraformVars: map[string]interface{}{ // Limited gen2 to Montreal
 			"region":             "ca-mon",
 			"plan":               "standard-gen2",
-			"postgresql_version": "18",
+			"postgresql_version": latestVersion,
 			"service_endpoints":  "private",
 		},
 		CloudInfoService: sharedInfoSvc,
 	})
+
+	fmt.Println("Latest version is ", latestVersion)
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
