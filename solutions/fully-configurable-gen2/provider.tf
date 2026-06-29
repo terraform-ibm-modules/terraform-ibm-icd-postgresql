f41438290abfd94053ce090065f8b1b4d9d103a1
Harika <86517031+HarikaPonna@users.noreply.github.com>
2026-06-29 15:01:15 +0530

    Update solutions/fully-configurable-gen2/provider.tf
    Co-authored-by: Aashiq Jacob <122446118+Aashiq-J@users.noreply.github.com>

--- solutions/fully-configurable-gen2/provider.tf
+++ solutions/fully-configurable-gen2/provider.tf
@@ -4,7 +4,7 @@ provider "ibm" {
   visibility       = var.provider_visibility
 }
 
-# Provider block for KMS (Key Protect or HPCS)
+# Provider block for KMS (Key Protect or Key Protect Dedicated)
 provider "ibm" {
   alias            = "kms"
   ibmcloud_api_key = var.ibmcloud_kms_api_key != null ? var.ibmcloud_kms_api_key : var.ibmcloud_api_key
