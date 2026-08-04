b9b9459172bfefba931f9b60bc703646a0cfff03
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2026-08-04 14:11:28 +0530

    fix: S2S authorization policy cannot be sourced from a resource group for Gen2 instances

--- main.tf
+++ main.tf
@@ -73,7 +73,7 @@ locals {
 resource "ibm_iam_authorization_policy" "kms_policy" {
   count                    = local.create_kms_auth_policy
   source_service_name      = "databases-for-postgresql"
-  source_resource_group_id = var.resource_group_id
+  source_resource_group_id = local.is_classic ? var.resource_group_id : null
   roles                    = ["Reader", "Authorization Delegator"] # Authorization Delegator role required for backup encryption key
   description              = "Allow all PostgreSQL instances in the resource group ${var.resource_group_id} to read the ${local.kms_service} key ${local.kms_key_id} from the instance GUID ${local.kms_key_instance_guid}"
   resource_attributes {
